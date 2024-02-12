828ba6bbf2ef17b88c0661434015305f1b7df9d5
Rodrigo Aguilar García <rodrigo.aguilar@iuv.edu.mx>
2024-02-11 20:26:24 -0600

    v0.0.1 [2.5]
    ForceSchema

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Routing\UrlGenerator;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -21,7 +22,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot(UrlGenerator $url): void
     {
         if (env('REDIRECT_HTTPS', false)) {
-            $url->formatScheme('https://');
+            $url->forceScheme('https://');
         }
     }
 }
