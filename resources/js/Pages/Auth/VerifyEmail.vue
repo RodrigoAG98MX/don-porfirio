72d606cced65119b4f467d6c8ed55f5dd8f6a4c1
Rodrigo Aguilar García <rodrigo.aguilar@iuv.edu.mx>
2024-02-11 19:45:52 -0600

    v0.0.1 [2.3] FixComponents
    Comment components

--- resources/js/Pages/Auth/VerifyEmail.vue
+++ resources/js/Pages/Auth/VerifyEmail.vue
@@ -1,8 +1,8 @@
 <script setup>
-import { computed } from 'vue';
+import {computed} from 'vue';
 import GuestLayout from '@/Layouts/GuestLayout.vue';
-import PrimaryButton from '@/Components/PrimaryButton.vue';
-import { Head, Link, useForm } from '@inertiajs/vue3';
+//import PrimaryButton from '@/Components/PrimaryButton.vue';
+import {Head, Link, useForm} from '@inertiajs/vue3';
 
 const props = defineProps({
     status: {
@@ -21,7 +21,7 @@ const verificationLinkSent = computed(() => props.status === 'verification-link-
 
 <template>
     <GuestLayout>
-        <Head title="Email Verification" />
+        <Head title="Email Verification"/>
 
         <div class="mb-4 text-sm text-gray-600 dark:text-gray-400">
             Thanks for signing up! Before getting started, could you verify your email address by clicking on the link
@@ -34,16 +34,17 @@ const verificationLinkSent = computed(() => props.status === 'verification-link-
 
         <form @submit.prevent="submit">
             <div class="mt-4 flex items-center justify-between">
-                <PrimaryButton :class="{ 'opacity-25': form.processing }" :disabled="form.processing">
-                    Resend Verification Email
-                </PrimaryButton>
+                <!--                <PrimaryButton :class="{ 'opacity-25': form.processing }" :disabled="form.processing">
+                                    Resend Verification Email
+                                </PrimaryButton>-->
 
                 <Link
                     :href="route('logout')"
                     method="post"
                     as="button"
                     class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800"
-                    >Log Out</Link
+                >Log Out
+                </Link
                 >
             </div>
         </form>
